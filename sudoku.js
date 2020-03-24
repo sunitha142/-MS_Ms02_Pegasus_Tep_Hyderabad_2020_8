b4f966729f13ccb83ad0ae145364a497f38c13f4
Aniket-ap <aniket_pro@outlook.com>
2020-03-24 14:32:34 +0530

    Final Code(Changed the code Format)

--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,31 @@
 let board = [
-    [0,0,0,2,6,0,7,0,1],
-    [6,8,0,0,7,0,0,9,0],
-    [1,9,0,0,0,4,5,0,0],
-    [8,2,0,1,0,0,0,4,0],
-    [0,0,4,6,0,2,9,0,0],
-    [0,5,0,0,0,3,0,2,8],
-    [0,0,9,3,0,0,0,7,4],
-    [0,4,0,0,5,0,0,3,6],
-    [7,0,3,0,1,8,0,0,0]
-]
+  [0, 0, 0, 2, 6, 0, 7, 0, 1],
+  [6, 8, 0, 0, 7, 0, 0, 9, 0],
+  [1, 9, 0, 0, 0, 4, 5, 0, 0],
+  [8, 2, 0, 1, 0, 0, 0, 4, 0],
+  [0, 0, 4, 6, 0, 2, 9, 0, 0],
+  [0, 5, 0, 0, 0, 3, 0, 2, 8],
+  [0, 0, 9, 3, 0, 0, 0, 7, 4],
+  [0, 4, 0, 0, 5, 0, 0, 3, 6],
+  [7, 0, 3, 0, 1, 8, 0, 0, 0]
+];
 
 // possible() determines wheather on a certain position, we can put the number
-function possible(y, x, n) {  
-  for (let i = 0; i < board.length; i++){
+function possible(y, x, n) {
+  for (let i = 0; i < board.length; i++) {
     if (board[y][i] == n) {
       return false;
     }
   }
-  for (let i = 0; i < board.length; i++){
+  for (let i = 0; i < board.length; i++) {
     if (board[i][x] == n) {
       return false;
     }
   }
 
   let x0 = Math.floor(x / 3) * 3;
-  let y0 = Math.floor(y / 3) * 3 
-  
+  let y0 = Math.floor(y / 3) * 3;
+
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (board[y0 + i][x0 + j] == n) {
@@ -36,24 +36,23 @@ function possible(y, x, n) {
   return true;
 }
 
-//console.log(possible(4, 4, 5))
-
-function solve(){
+function solve() {
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board.length; x++) {
-      if (board[y][x] == 0){
-        for (let n = 1; n <= board.length; n++){
+      if (board[y][x] == 0) {
+        for (let n = 1; n <= board.length; n++) {
           if (possible(y, x, n)) {
-            board[y][x] = n
-            solve()
-            board[y][x] = 0
+            board[y][x] = n;
+            solve();
+          } else {
+            board[y][x] = 0;
           }
         }
-        return ;
+        return;
       }
     }
   }
-  console.log(board)
+  console.log(board);
 }
 
-solve()
+solve();
